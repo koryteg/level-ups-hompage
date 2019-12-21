60f365725075b0272770fa3bca9342765ddc9394
kory tegman <kory@echobind.com>
2019-12-20 16:40:38 -0800

    initial commit with homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Level Ups DOT dev`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,32 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import LightningBolt from "../components/lightning-bolt"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="Level Up you Dev Skillzzzzz" />
+    <h1>Level Up Your Dev Skills</h1>
+    <p>
+      Welcome Levelups.dev. Where we will make you a better programmer. by
+      leveling you up at all kinds of software development skills.
+    </p>
+    <p>
+      we will cover languages and topics such as: Ruby, React, Dev Ops, Web
+      developemnt, CSS, Deployment, Career Success.
+    </p>
+    <LightningBolt fill={"#ffe500"} />
+    {/* <Image /> */}
   </Layout>
 )
 
 export default IndexPage
+
+// colors
+// #ffe500
+// #000000
+// #00a6fb
+// #0582ca
+// #006494
